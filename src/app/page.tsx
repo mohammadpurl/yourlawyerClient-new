6b5bc808595aaa8794760892838f52d40de58f4f
leila-P <leilapoormasti@gmail.com>
2024-07-21 03:45:59 -0700

    add first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,9 +100,12 @@ export default function Home() {
         </div>
 
         {/* steps */}
-        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+        <ol
+          className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0"
+          dir="rtl"
+        >
           <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+            <div className="flex flex-col space-y-2 border-l-4 md:p-x-12 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
               <span className="text-sm font-medium text-blue-600">گام اول</span>
               <span className="text-xl font-semibold">ثبت نام کنید</span>
               <span className="mt-2 text-zinc-700">
